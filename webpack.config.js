8e3c28754fde9b8cd143ae11e3f5187c0646f099
Seokyung Jung <tjrud1438@naver.com>
2021-09-23 23:19:43 +0900

    webpack dev server 설정

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
 const path = require('path')
 
 module.exports = {
+  mode: 'development',
   entry: './src/selection.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist')
+    },
+    compress: true,
+    port: 8001
   }
 }
